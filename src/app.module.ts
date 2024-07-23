5c5b4371b777d8e121cda7c7c57cffcfd1a83c0b
bryan lopez <blopez4434@gmail.com>
2024-07-23 16:30:10 -0400

    add worker model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AreaModule } from './area/area.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { WorkerModule } from './worker/worker.module';
 
 @Module({
-  imports: [PrismaModule, AreaModule],
+  imports: [PrismaModule, AreaModule, WorkerModule],
   controllers: [],
   providers: [],
 })
